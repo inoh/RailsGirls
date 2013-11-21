467832c95acd12097b9fade7b8ace7d9b036bbce
Hiroyuki Inoue <syslink.h.inoue@gmail.com>
2013-11-21 16:48:48 +0900

    講師用スライドを手直し

--- slide/lib/default.rb
+++ slide/lib/default.rb
@@ -1,2 +1,13 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+
+include Nanoc::Helpers::Blogging
+include Nanoc::Helpers::Breadcrumbs
+include Nanoc::Helpers::Capturing
+include Nanoc::Helpers::Filtering
+include Nanoc::Helpers::HTMLEscape
+include Nanoc::Helpers::LinkTo
+include Nanoc::Helpers::Rendering
+include Nanoc::Helpers::Tagging
+include Nanoc::Helpers::Text
+include Nanoc::Helpers::XMLSitemap
